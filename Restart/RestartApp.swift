73aa3633abae95f1d3b8dd48680f6aa8f829c33c
Light2288 <davide.aliti@gmail.com>
2022-03-17 22:42:29 +0100

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Davide Aliti on 17/03/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
